742bfd504e222aa21a86c43edab524d0783c8b03
abdeladimboukrim <boukrimabdeladim@email.com>
2021-02-26 14:07:28 +0100

    EXercice 8

---
+++ Exercice-8/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$info = array('nom' => "boukrim",'prenom' => "abdeladim",'age' => 25 ,'sex' => "homme",'ville' => "youssoufia",'origin' => "Youssoufia");
+
+foreach ($info as $formation => $devWeb) {
+  echo "<ul>
+
+  <li> $formation : $devWeb . <br></li>
+
+  </ul>";
+  // code...
+}
+
+
+?>
